5713f31e14f54702961bf59eb7cd6513b0cdef51
yigitsenertr <info.yigitsener@gmail.com>
2026-05-30 17:08:15 +0300

    ilk commit - devops lab

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+﻿FROM python:3.11-slim
+WORKDIR /app
+COPY app/requirements.txt .
+RUN pip install -r requirements.txt
+COPY app/ .
+EXPOSE 5000
+CMD ["python", "app.py"]
---
+++ app/app.py
@@ -0,0 +1,19 @@
+﻿from flask import Flask, render_template
+import datetime
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', 
+        time=datetime.datetime.now().strftime("%H:%M:%S"),
+        env=os.getenv("APP_ENV", "development")
+    )
+
+@app.route('/health')
+def health():
+    return {"status": "ok"}, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
